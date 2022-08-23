b83272f223a57c8a3aba104a9f6ef710ae906dd7
SifisoDhlamini <sifisoldhlamini@outlook.com>
2022-08-23 21:07:13 +0800

    First 10 tests past. Spend chart pending.

--- budget-app/budget.py
+++ budget-app/budget.py
@@ -1,14 +1,13 @@
 class Category:
     def __init__(self, name):
         self.name = name
-        self.ledger = []
-    
-    @property
+        self.ledger = []    
+
     def get_balance(self):
         return sum(entry["amount"] for entry in self.ledger)
     
     def check_funds(self, amount):
-        return self.balance >= amount
+        return self.get_balance() >= amount
     
     def deposit(self, amount, description=""):
         self.ledger.append({"amount": amount, "description": description})
@@ -26,6 +25,21 @@ class Category:
             return True
         return False
 
+    def __str__(self) -> str:
+        title = f"{self.name.center(30, '*')}\n"
+        transactions = []
+        for transaction in self.ledger:
+            amount = "{:.2f}".format(transaction["amount"])
+            right = len(str(amount)) + 1
+            description = transaction["description"][:30 - right]
+            transactions.append(f"{description.ljust(30-right)}{amount.rjust(right)}\n")
+        transactions = "".join(transactions)     
+        total = f"Total: {self.get_balance():.2f}"
+
+        return title + transactions + total
+        
+
 
 def create_spend_chart(categories):
     ...
+    
